971008a4d0caa086f6833a67e975ae6abca8b63a
Jiwoo Jung <wldn3634@naver.com>
2017-10-19 21:13:43 +0900

    페이스북에서 사진 url 가져와서 설정 화면의 사진을 사용자 페이스북 사진으로 설정 성공~~~^^

--- app/src/main/java/mokpoharbor/ringring/MainActivity.java
+++ app/src/main/java/mokpoharbor/ringring/MainActivity.java
@@ -83,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                intent.putExtra("image_url", user_image_url);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/mokpoharbor/ringring/SettingActivity.java
+++ app/src/main/java/mokpoharbor/ringring/SettingActivity.java
@@ -1,16 +1,26 @@
 package mokpoharbor.ringring;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Created by pingrae on 2017. 10. 8..
  */
 
 public class SettingActivity extends AppCompatActivity {
+
+    private String user_image_url;
+
+    Handler handler = new Handler();
 
     BackPressClose back_pressed;
 
@@ -27,6 +37,37 @@ public class SettingActivity extends AppCompatActivity {
 
         //액티비티 타이틀바 내용 설정
         setTitle("SETTING");
+
+
+        Bundle i = getIntent().getExtras();
+        user_image_url= i.getString("image_url");
+
+        Thread t =new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    final ImageView iv = (ImageView)findViewById(R.id.user_image);
+                    URL url = new URL(user_image_url);
+                    InputStream is = url.openStream();
+                    final Bitmap bm = BitmapFactory.decodeStream(is);
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {  // 화면에 그려줄 작업
+                            iv.setImageBitmap(bm);
+                        }
+                    });
+                    iv.setImageBitmap(bm); //비트맵 객체로 보여주기
+                } catch(Exception e){
+
+                }
+
+            }
+
+        });
+
+        t.start();
+
 
         //뒤로가기 버튼 눌를시 토스트메세지로 확인 메세지를 뛰어준다
         back_pressed = new BackPressClose(this);
