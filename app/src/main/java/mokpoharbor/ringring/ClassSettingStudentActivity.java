84087300617f3c1a0f5dfdde005604f1a0203059
Jiwoo Jung <wldn3634@naver.com>
2017-10-21 04:20:14 +0900

    학생 강좌 설정 화면에 알림 받을지 말건지, 알람 주기, 수강강좌목록 보는 버튼, 수강 강좌 등록하는 버튼을 만들었다. 알람 주기 버튼을 누르면 시계를 뛰어 알림 주기를 선택할 수 있고 그 값은 저장되어 텍스트에 보여진다.
    교수 설정 화면도 만들었다.
    아직 기능들은 미완성

--- app/src/main/java/mokpoharbor/ringring/ClassSettingStudentActivity.java
+++ app/src/main/java/mokpoharbor/ringring/ClassSettingStudentActivity.java
@@ -1,7 +1,11 @@
 package mokpoharbor.ringring;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by pingrae on 2017. 10. 20..
@@ -16,6 +20,27 @@ public class ClassSettingStudentActivity extends AppCompatActivity {
 
         //액티비티 타이틀바 내용 설정
         setTitle("Class Setting");
+
+        Button set_alram_cycle = (Button) findViewById(R.id.set_alram_cycle);
+        set_alram_cycle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetAlramCycleFragment set_alram_fragment = new SetAlramCycleFragment();
+                set_alram_fragment.show(getFragmentManager(), "TimePicker");
+            }
+        });
+
+        TextView alram_cycle = (TextView)findViewById(R.id.arlam_cycle);
+
+        SharedPreferences pref = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String alram_hour = pref.getString("alram_hour", "nothing");
+        String alram_minute = pref.getString("alram_minute", "nothing");
+
+        if(!alram_hour.equals("nothing")){
+
+            alram_cycle.setText(alram_hour + "시간 " + alram_minute + "분");
+
+        }
     }
 }
 
---
+++ app/src/main/java/mokpoharbor/ringring/SetAlramCycleFragment.java
@@ -0,0 +1,54 @@
+package mokpoharbor.ringring;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
+
+import static android.content.Context.MODE_PRIVATE;
+
+/**
+ * Created by pingrae on 2017. 10. 21..
+ */
+
+public class SetAlramCycleFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        final Calendar c = Calendar.getInstance();
+
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        TimePickerDialog tpd = new TimePickerDialog(getActivity(), AlertDialog.THEME_DEVICE_DEFAULT_DARK, this, hour, minute, true);
+
+        TextView tvTitle = new TextView(getActivity());
+        tvTitle.setText("알림 주기 설정");
+        //tvTitle.setBackgroundColor();
+        tvTitle.setPadding(5, 3, 5, 3);
+        tvTitle.setGravity(Gravity.CENTER_HORIZONTAL);
+        tpd.setCustomTitle(tvTitle);
+        return tpd;
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hour, int minute){
+        TextView tv = (TextView) getActivity().findViewById(R.id.arlam_cycle);
+        tv.setText(String.valueOf(hour) + "시간 " + String.valueOf(minute) + "분");
+
+        SharedPreferences pref = this.getActivity().getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putString("alram_hour", String.valueOf(hour));
+        editor.putString("alram_minute", String.valueOf(minute));
+        editor.commit();
+
+    }
+}
