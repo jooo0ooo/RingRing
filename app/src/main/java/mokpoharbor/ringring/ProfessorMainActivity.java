bbe1f7404dbaeafd295abb5e1f3364f3035d440c
Jiwoo Jung <wldn3634@naver.com>
2017-10-22 20:54:39 +0900

    혁진이형이 만든 이미지 파일 2개 첨부 처음 어플 킬때 splash 화면 바꿔봄

--- app/src/main/java/mokpoharbor/ringring/ProfessorMainActivity.java
+++ app/src/main/java/mokpoharbor/ringring/ProfessorMainActivity.java
@@ -1,10 +1,13 @@
 package mokpoharbor.ringring;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ProfessorMainActivity extends AppCompatActivity {
 
@@ -58,6 +61,42 @@ public class ProfessorMainActivity extends AppCompatActivity {
 
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        ImageView hand_over_subject = (ImageView)findViewById(R.id.hand_over_subject);
+        hand_over_subject.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                AlertDialog.Builder alertdialog = new AlertDialog.Builder(ProfessorMainActivity.this);
+                //다이얼로그의 내용을 설정합니다.
+                alertdialog.setTitle("Warning!");
+                alertdialog.setMessage("과제를 생성하시면 학생들에게 미움받을 수 있습니다.\n미움받을 용기 있으십니까?");
+
+                //확인 버튼
+                alertdialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //확인 버튼이 눌렸을 때 토스트를 띄워줍니다.
+                        Toast.makeText(ProfessorMainActivity.this, "구현 준비 중 입니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                //취소 버튼
+                alertdialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //취소 버튼이 눌렸을 때 토스트를 띄워줍니다.
+                        Toast.makeText(ProfessorMainActivity.this, "구현 준비 중 입니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alert = alertdialog.create();
+                alert.show();
+
+
             }
         });
 
